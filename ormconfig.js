ecb88bb825b1ba74c2dadc1be99dc48db88ad05a
Dusan <dusan.spremo.17@singimail.rs>
2021-09-08 19:35:38 +0100

    setovanje depedencija u package jsonu

---
+++ ormconfig.js
@@ -0,0 +1,20 @@
+module.exports = {
+    type: 'mysql',
+    host: process.env.DB_HOST || '127.0.0.1',
+    username: process.env.DB_USER || 'dusanspremo17',
+    password: process.env.DB_PASSWORD || 'password1',
+    database: process.env.DB_NAME || 'Aplikacija',
+    charset: 'utf8',
+    driver: 'mysql',
+    synchronize: false ,
+    entities:[
+        "src/entity/**/*.ts"
+    ],
+    logging: true ,
+    migrations: ["src/migration/**/*.ts"],
+    cli: {
+      migrationsDir: "migration"
+    },
+    connectTimeout: 30000,
+    acquireTimeout: 30000
+  };
